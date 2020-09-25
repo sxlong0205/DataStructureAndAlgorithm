eb2396236ce9dc0fc9be33a01d4e835b16826829
xlshi <942309989@qq.com>
2020-09-25 14:31:42 +0800

    update Heap HeapSort

--- src/Heap.java
+++ src/Heap.java
@@ -2,10 +2,69 @@
  * @author : Code Dragon
  * create at:  2020/9/25  11:30
  */
-public class Heap<T extends Comparable<T>> extends Sort<T> {
+public class Heap<T extends Comparable<T>> {
+    //存放堆中的元素
+    private T[] items;
+    //堆中元素个数
+    private int N;
 
-    @Override
-    public void sort(T[] nums) {
-        
+    public Heap(int capacity) {
+        items = (T[]) new Comparable[capacity + 1];
+        N = 0;
+    }
+
+    private boolean less(int i, int j) {
+        return items[i].compareTo(items[j]) < 0;
+    }
+
+    private void exch(int i, int j) {
+        T temp = items[i];
+        items[i] = items[j];
+        items[j] = temp;
+    }
+
+    //插入一个新元素
+    public void insert(T t) {
+        items[++N] = t;
+        swim(N);
+    }
+
+    //下沉操作
+    private void swim(int k) {
+        //如果到根节点则退出循环
+        while (k > 1 && less(k / 2, k)) {//如果父节点小于根节点，两者交换
+            exch(k / 2, k);
+            k /= 2;
+        }
+    }
+
+    //删除堆中最大元素并返回
+    public T delMax() {
+        T max = items[1];
+        //将最后一个元素放到堆顶
+        exch(1, N--);
+        //将堆最后一个位置置空
+        items[N + 1] = null;
+        //将堆顶元素下沉到合适的位置
+        sink(1);
+        return max;
+    }
+
+    //上浮操作
+    private void sink(int k) {
+        //到堆底元素时结束循环
+        while (2 * k <= N) {
+            //将左孩子赋值给max
+            int max = 2 * k;
+            if (max < N && less(max, max + 1)) {   //如果左孩子比右孩子小，则将右孩子赋值给max
+                max++;
+            }
+            if (!less(k, max)) {
+                break;
+            }
+            //如果父节点小于两个孩子中较大的结点，则交换
+            exch(k, max);
+            k = max;
+        }
     }
 }
--- src/HeapSort.java
+++ src/HeapSort.java
@@ -5,6 +5,33 @@
 public class HeapSort<T extends Comparable<T>> extends Sort<T> {
     @Override
     public void sort(T[] nums) {
+        int N = nums.length - 1;
+        //从第一个非孩子结点开始遍历，调整排序
+        for (int k = N / 2; k >= 1; k--) {
+            sink(nums, k, N);
+        }
 
+        while (N > 1) {
+            swap(nums, 1, N--);
+            sink(nums, 1, N);
+        }
+    }
+
+    private void sink(T[] nums, int k, int N) {
+        while (2 * k <= N) {
+            int max = 2 * k;
+            if (max < N && less(nums, max, max + 1)) {
+                max++;
+            }
+            if (!less(nums, k, max)) {
+                break;
+            }
+            swap(nums, k, max);
+            k = max;
+        }
+    }
+
+    private boolean less(T[] nums, int k, int max) {
+        return nums[k].compareTo(nums[max]) < 0;
     }
 }
